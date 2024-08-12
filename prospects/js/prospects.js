2e6d60d793dc46bad23d1d3fa70d67173ab6b093
cybermaxi7 <maximusagbe>
2024-08-12 21:58:56 +0100

    finished working on the create prospect pagpage

---
+++ prospects/js/prospects.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const createProspectBtn = document.querySelector(".create-prospect");
+    createProspectBtn.addEventListener("click", function () {
+        window.location.href = "/prospects/create-prospect.html";
+    });
+});
